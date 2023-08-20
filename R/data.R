c7c5eb750fcf2d91a414423f0fe6fcf1fba92eb6
Enrico R. Crema <enrico.crema@gmai.com>
2023-08-20 09:52:45 +0100

    Added sample data

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' @title Sample aoristic data (data.frame)
+#' @description Sample datasets to illustrate data formats required for \code{createProbMat()}.
+#' @format A data.frame class object with two columns defining the start and the end of each even (\code{sample.df}) 
+#' @examples
+#' \dontrun{
+#' data(sampledf)
+#' x  <- createProbMat(x=sample.df,timeRange = c(6500,4001),resolution= 100)
+#' plot(x)
+#'}
+"sampledf"
+
+#' @title Sample aoristic data (matrix)
+#' @description Sample datasets to illustrate data formats required for \code{createProbMat()}.
+#' @format A matrix class object storing the probability of each event (row) in each time-block (column) 
+#' @examples
+#' \dontrun{
+#' data(samplemat)
+#' x <- createProbMat(pmat=samplemat,timeRange = c(5000,3001),resolution=100)
+#' plot(x)
+#'}
+"samplemat"
