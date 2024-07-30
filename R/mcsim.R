1ed49af5df646dc3aec1614bdf519089653fb8ac
Enrico Crema <enrico.crema@gmail.com>
2024-07-30 11:29:56 +0100

    Fixed typos

--- R/mcsim.R
+++ R/mcsim.R
@@ -3,7 +3,7 @@
 #' @param x A ProbMat class object
 #' @param nsim Number of Monte-Carlo simulations
 #' @details  The function randomly assigns to each event a time-block based on its probability values (i.e. aoristic weight) and computes, for each time-block, the total number of simulated events. This process is repeated \code{nsim} time, allowing to estimate percentile-based intervals on the number of events per time-block (Crema 2012). It should be noted that while this approach accounts for chronological uncertainty, it provides only a description of the sample rather than the underlying population, and can be biased how the underlying archaeological periodisations define the time-spans of each event (see also Crema 2024 for discussion on limitations).
-#' @return  An objcect of class \code{mcsimres} containing relevant metadata and a matrix with the number of events per time-block per Monte-Carlo simulation. 
+#' @return  An object of class \code{mcsimres} containing relevant metadata and a matrix with the number of events per time-block per Monte-Carlo simulation. 
 #' @references 
 #
 #' Crema, E. R. (2012). Modelling Temporal Uncertainty in Archaeological Analysis. Journal of Archaeological Method and Theory, 19(3), 440–461. doi:10.1007/s10816-011-9122-3
